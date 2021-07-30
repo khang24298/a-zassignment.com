300930323b2d39f0bb789f8f4800318d672b37d6
khang24298 <khangnguyen24298@gmail.com>
2021-07-30 21:31:05 +0700

    Add database host

---
+++ lib/dbCon.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "azassignment.crm9yo3gf2n6.us-east-2.rds.amazonaws.com";
+$database = "thenikki";
+$username = "admin";
+$password = "Cxhkt1998!";
+
+$mysqli = new mysqli($servername, $username, $password, $database);
+    
+  /* check connection */
+  if (mysqli_connect_errno()) {
+      printf("Connect failed: %s\n", mysqli_connect_error());
+      exit();
+  }
+    
+  /* change character set to utf8 */
+  if (!$mysqli->set_charset("utf8")) {
+    //   printf("Error loading character set utf8: %s\n", $mysqli->error);
+  } else {
+    //   printf("Current character set: %s\n", $mysqli->character_set_name());
+  }
+    
+$mysqli->close();
+?>
