64b50176a8a36855f4b9c22e6a71edd28adf70d5
khang24298 <khangnguyen24298@gmail.com>
2021-07-31 00:23:23 +0700

    Add database host

--- lib/dbCon.php
+++ lib/dbCon.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "azassignment.crm9yo3gf2n6.us-east-2.rds.amazonaws.com";
-$database = "a_zassignment";
+$database = "a_zassignement";
 $username = "admin";
 $password = "Cxhkt1998!";
 
 $mysqli = new mysqli($servername, $username, $password, $database);
-// Specify connection timeout
-$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 30);
+    
   /* check connection */
   if (mysqli_connect_errno()) {
       printf("Connect failed: %s\n", mysqli_connect_error());
